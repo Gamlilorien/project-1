df1ca2c3a4a335358a9500d35dd470ffaba2ebf1
Drakeen <info@howardfms.com>
2018-11-24 12:20:27 -0700

    basic account page and active nav button

---
+++ assets/javascript/layout.js
@@ -0,0 +1,45 @@
+//To override Datepicker default formatting
+inter_es = {
+  months:    [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec'
+  ]
+};
+
+
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+    $('.modal').modal();
+    $('select').formSelect();
+    $('.datepicker').datepicker({
+      format: "mm/dd/yy",
+      autoClose: true,
+      defaultDate: 1/1/2018,
+      setDefaultDate: true,
+      months: [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec'
+      ],
+      i18n: inter_es
+    });
+  });

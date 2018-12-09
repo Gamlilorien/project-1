74c8dd07f8305a81d27e722cc1aa52088baa0d89
Kelsey <kelsgainer@gmail.com>
2018-12-09 15:55:52 -0700

    new weather

--- assets/javascript/weather-api.js
+++ assets/javascript/weather-api.js
@@ -1,6 +1,31 @@
-var queryURL = "https://api.weatherbit.io/v2.0/lat=38.123&lon=-78.543&key=d242172e1f4b4afbb2d2c815f4043f71" 
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
+var weatherData = [""];
+
+$(document).on("click", "submit", function() {
+  event.preventDefault()
+
+  // lat&lng api link: Other link is just for testing
+  // var queryURL = "https://api.weatherbit.io/v2.0/lat=&lon=&key=d242172e1f4b4afbb2d2c815f4043f71" 
+  var queryURL = "https://api.weatherbit.io/v2.0/current?city=Raleigh,NC&key=d242172e1f4b4afbb2d2c815f4043f71" 
+
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    $("#weather-info").text(JSON.stringify(response));
+    console.log(response);
+    
+  });
+
+  var temp = "temp"
+  var icons = "weather"
+  var results = response.data
+
+  for(var i = 0; i < weatherData; i++)
+  console.log(weatherData);
+  
+
 });
+
+
--- weather-api.js
+++ weather-api.js
@@ -1,6 +1,206 @@
-var queryURL = "https://api.weatherbit.io/v2.0/lat=38.123&lon=-78.543&key=d242172e1f4b4afbb2d2c815f4043f71" 
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-});
+  
+  var lat
+  var lng 
+  console.log("lat", lat, "lng", lng)
+
+  var queryURL = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lng + "&units=imperial&appid=9eabcaeeafc5beb1cdf26fdd62664be2";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (data) {
+    var results = data    
+      
+    var temperature = Math.round(results.main.temp);
+    var desc = results.weather[0].description;
+    var iconCode = results.weather[0].icon;
+    console.log(iconCode);
+
+    var iconImg = "http://openweathermap.org/img/w/" + iconCode + ".png"
+    console.log(iconImg);
+    
+
+    $("#temperature").html(temperature);
+    $("#weather-desc").html(desc);
+    $("#weather-icon").html("src=" + iconImg);
+
+    // I wanted to organize these by condition but ran out of time, will do so at a later date
+    switch(results.weather[0].icon) {
+
+      // thunderstorm
+      case "thunderstorm with light rain":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "thunderstorm with rain":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "thunderstorm with heavy rain":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "light thunderstorm":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "thunderstrom":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "heavy thunderstorm":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "ragged thunderstorm":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "thunderstorm with light drizzle":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "thunderstorm with drizzle":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "thunderstorm with heavy drizzle":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+
+      // snow
+      case "light snow":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "snow":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "heavy snow":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "sleet":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "shower sleet":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "light rain and snow":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "light shower snow":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "shower snow":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "heavy shower snow":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+
+      // Drizzle
+      case "light intensity drizzle":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "drizzle":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "heavy intensity drizzle":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "light intensity drizzle rain":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "drizzle rain":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "heavy intensity drizzle rain":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "shower rain and drizzle":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "heavy shower rain and drizzle":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "shower drizzle":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+
+      // rain
+      case "light rain":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "moderate rain":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "heavy intensity rain":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "very heavy rain":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "extreme rain":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "freezing rain":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "light intensity shower rain":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "shower rain":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "heavy intensity shower rain":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "ragged shower rain":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+
+      // atmosphere
+      case "mist":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "smoke":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "haze":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "sand, dust whirls":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "fog":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "sand":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "dust":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "volcanic ash":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "squalls":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "tornado":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+
+      // clear & clouds
+      case "clear sky":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "few clouds":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "scattered clouds":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "broken clouds":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+      case "overcast clouds":
+      $("#weather-icon").attr("src", iconImg);
+      break;
+    }
+
+  });
+
+
+
